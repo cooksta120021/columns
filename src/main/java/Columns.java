0a6d6b8cc92b19270f95ebb6d5133d8c1b7c638d
Jahmeira <jahmeira.ruffin@genspark.net>
2022-01-11 12:31:20 -0500

    Updated 12:31EST

---
+++ src/main/java/Columns.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Random;
+//import java.util.stream.Collectors;
+
+public class Columns {
+    //This file or section will be about reading the input file
+    //Code only reading from 1st line
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("Input.txt")));
+        Random random = new Random();
+
+        // Calls input from writer and creates new lines
+       // String line = reader.lines().collect(Collectors.joining(System.lineSeparator()));
+      //  System.out.println(line); // Reads file
+        reader.close();
+    }
+}
+
+
--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -1,2 +1,27 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class Input {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner input = new Scanner(System.in);
+        String text;
+
+        System.out.println("write 1-20 in words");
+        text = input.nextLine();
+        input.close();
+        System.out.println(text);
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(new File("Input.txt"),true));
+            writer.append(text);
+            writer.newLine();
+            System.out.println("Adding to File " + "Input.txt");
+            writer.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // write.ReadLine();
+    }
+
 }
