37173a0977442a7abc46c661e0f919650242c851
Jahmeira <jahmeira.ruffin@genspark.net>
2022-01-13 12:46:56 -0500

    12:46PM EST

--- src/main/java/Columns.java
+++ src/main/java/Columns.java
@@ -3,18 +3,18 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Random;
-//import java.util.stream.Collectors;
+import java.util.stream.Collectors;
 
 public class Columns {
     //This file or section will be about reading the input file
     //Code only reading from 1st line
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(new File("Input.txt")));
+        BufferedReader reader = new BufferedReader(new FileReader("Input.txt"));
         Random random = new Random();
 
         // Calls input from writer and creates new lines
-       // String line = reader.lines().collect(Collectors.joining(System.lineSeparator()));
-      //  System.out.println(line); // Reads file
+        String line = reader.lines().collect(Collectors.joining(System.lineSeparator()));
+        System.out.println(line); // Reads file
         reader.close();
     }
 }
