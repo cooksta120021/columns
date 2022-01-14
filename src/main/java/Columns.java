585656f0206ef3dd9e4595f37db32d5efdc85ce3
Jahmeira <jahmeira.ruffin@genspark.net>
2022-01-14 13:29:58 -0500

    1:29PM EST

--- src/main/java/Columns.java
+++ src/main/java/Columns.java
@@ -9,14 +9,23 @@ public class Columns {
     //This file or section will be about reading the input file
     //Code only reading from 1st line
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader("Input.txt"));
-        Random random = new Random();
+       try {
+           BufferedReader reader = new BufferedReader(new FileReader("Input.txt"));
+           Random random = new Random();
 
-        // Calls input from writer and creates new lines
-        String line = reader.lines().collect(Collectors.joining(System.lineSeparator()));
-        System.out.println(String.format("%-30s %s", line)); // Reads file
-        reader.close();
+           // Calls input from writer and creates new lines
+           String line = reader.lines().collect(Collectors.joining(System.lineSeparator()));
+           System.out.printf(String.format("%s%s", line.substring(0,0), line.substring(1,1)));
+            /*  System.out.printf("%-30s %s", line, line,line); // Reads file
+           System.out.printf("%-30s %s", line, "%-30s %s",line,"%-30s %s",line); // Reads file
+           System.out.printf("%-30s %s", line, line,line); // Reads file
+           System.out.printf("%-30s %s", line, line,line); // Reads file
+           System.out.printf("%-30s %s", line, line,line); // Reads file */
+           reader.close();
+       }
+       catch(Exception e){
 
+       }
        // String.format("%-30s %s", line);
     }
 }
